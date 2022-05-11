65abc8fdab4d40838789e9cff538bdad3d661a33
Sohel <absohel47@gmail.com>
2022-05-11 18:07:31 +0600

    Network Class initiated

---
+++ Netflix Clone/Managers/APICaller.swift
@@ -0,0 +1,18 @@
+//
+//  APICaller.swift
+//  Netflix Clone
+//
+//  Created by Abdullah Al Sohel on 5/11/22.
+//
+
+import UIKit
+
+class APICaller {
+    
+    static let shared = APICaller()
+    private let baseURL = "https://api.themoviedb.org"
+    private let API_KEY = "d537efe84ac70e422039c632f3907c23"
+    
+    private init () {}
+    
+}
